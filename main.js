4bc8c1503c6e5c430b8c9751a154275ba0ea2322
Carter N <124218362+cneumann01@users.noreply.github.com>
2025-06-03 19:37:58 +0000

    Working on projections

--- main.js
+++ main.js
@@ -7,4 +7,4 @@ const renderer = new Renderer(canvas);
 const p1 = new Vector3(100, 100);
 const p2 = new Vector3(300, 300);
 
-renderer.drawSquare(p1, p2, "orange");
+renderer.drawPoint(p1, p2, "orange");
--- renderer.js
+++ renderer.js
@@ -1,13 +1,38 @@
 class Renderer {
-	constructor(canvas) {
+	constructor(canvas, focalLength = 100) {
 		this.canvas = canvas;
 		this.ctx = canvas.getContext("2d");
 		this.width = canvas.width;
 		this.height = canvas.height;
+		this.focalLength = focalLength;
 	}
 
 	clear() {
 		this.ctx.clearRect(0, 0, this.width, this.height);
+	}
+
+	project(Vector3) {
+		const { x, y, z } = Vector3;
+		if (z <= 0) return null; // Point behind or in the camera
+
+		const f = this.focalLength;
+		const screenX = (f * x) / z;
+		const screenY = (f * y) / z;
+
+		const canvasX = screenX + this.width / 2;
+		const canvasY = screenY + this.height / 2;
+
+		return { x: canvasX, y: canvasY };
+	}
+
+	drawPoint(Vector3, color = "green") {
+		const { x, y, z } = Vector3;
+		this.ctx.beginPath();
+		this.ctx.arc(500, 500, 20, 0, Math.PI * 2);
+		this.ctx.strokeStyle = color;
+		this.ctx.stroke();
+		this.ctx.fillStyle = color;
+		this.ctx.fill();
 	}
 
 	drawTriangle(p1, p2, p3, color = "blue") {
@@ -23,7 +48,6 @@ class Renderer {
 	}
 
 	drawSquare(p1, p2, color = "red") {
-		console.log("Drawing Square", p1, p2);
 		const topLeft = p1;
 		const topRight = new Vector3(p2.x, p1.y);
 		const bottomLeft = new Vector3(p1.x, p2.y);
